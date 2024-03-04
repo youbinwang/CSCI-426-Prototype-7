2f4235534a556b44956aecec4fb0e517597793e8
Jeremy Yiu <jeremyyiu.student@gmail.com>
2024-03-03 16:10:51 -0800

    Color Flash On Hit

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -7,9 +7,22 @@ public class TargetController : MonoBehaviour
     public enum TargetFace{Miss, Gold, Red, Blue, Black, White}
     [SerializeField]
     private TargetFace myScore;
-
+    private SpriteRenderer myRenderer;
+    private Color myColor;
+    void Start(){
+        myRenderer = GetComponent<SpriteRenderer>();
+        myColor = myRenderer.color;
+    }
     void OnCollisionEnter2D(Collision2D collision){
-
+        StartCoroutine(Flash());
         ScoreManager.instance.Score(myScore);
+    }
+    IEnumerator Flash(){
+        for(int i = 0;i<3;i++){
+            myRenderer.color = Color.green;
+            yield return new WaitForSeconds(0.2f);
+            myRenderer.color = myColor;
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
